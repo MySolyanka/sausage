34e1383e908db088430ef872c31cd988864dc7bb
rufovS <serg.rufov2012@yandex.ru>
2023-03-07 09:52:46 +0300

    Мелкие исправления в миграциях

---
+++ app/Models/UsersItegration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersItegration extends Model
+{
+    use HasFactory;
+}
